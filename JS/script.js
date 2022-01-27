1466578e549b73a636aaab8db77fdfe6753002d2
Dmitry <ragdak@gmail.com>
2022-01-27 13:28:30 +0500

    Условия и циклы

--- JS/script.js
+++ JS/script.js
@@ -9,12 +9,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ''),
-      b = prompt("На сколько его оцените?", ''),
-      с = prompt("Один из последних просмотренных фильмов?", ''),
-      d = prompt("На сколько его оцените?", '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ''),
+        b = prompt("На сколько его оцените?", '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[с] = d;
+    if (a != null && b != null &&  a != '' && b != '' && a.length < 50 && b.length < 50) {
+
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if(personalMovieDB.count <= 10){
+    console.log("Просмотрено мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30){
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
